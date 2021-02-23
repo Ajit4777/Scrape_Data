64afd6925258e12793f970566db9ce2401837e86
Ajit4777 <ajipsss4777@gmail.com>
2021-02-23 13:19:56 +0530

    Script Changed

--- scrap.py
+++ scrap.py
@@ -1,9 +1,21 @@
+#python3 scrap.py "product_name_1" "product_name_2" ...
+
 import requests
 import json
 import sys
 import mysql.connector
 
-
+""" 
+For SQL Connection
+------------------
+Parameters
+----------
+    -host
+    -user
+    -password
+    -database name
+    -password format
+"""
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -13,6 +25,17 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+
+"""
+GET PRODUCT
+-----------
+Parameters
+-----------
+    -name : takes the product name for scraping it from the respectively api.
+Returns
+-------
+    It return an array of JSON data, containg all the product details..
+"""
 
 def get_products(name):
         headers = {
@@ -31,16 +54,20 @@ def get_products(name):
         except Exception as e:
             print('Error in scraping data from Zenscrape: {}'.format(str(e)))
             return []
+
+# Query for Inserting the data into the database
 sql = "INSERT INTO Products (name, price) VALUES (%s, %s)"
+
+# It will take the product names from command line argument.
 n = len(sys.argv)
 for i in range(1,n):
     products = get_products(sys.argv[i])
-    print(products)
     products_json = json.dumps(products)
     products_data = json.loads(products_json)
     val = []
     for i in range(len(products_data)):
         val.append((products_data[i]['basic']['name'],products_data[i]['store']['price']['displayPrice']))
+        # print(products_data[i]['store']['price']['displayPrice'])
     mycursor.executemany(sql, val)
 
 mydb.commit()
